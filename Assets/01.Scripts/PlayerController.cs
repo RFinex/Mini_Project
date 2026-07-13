5c007e0a022d874f6f7e6b36d821febdea06998f
RFinex <leo97kr@gmail.com>
2026-07-13 10:36:17 +0900

    feat: Add Player Move

--- Assets/01.Scripts/PlayerController.cs
+++ Assets/01.Scripts/PlayerController.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody2D rb;
+    private Collider2D col;
+
+    private float dir;
+    private float speed;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+        speed = 5f;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        PlayerControll();
+    }
+
+    private void PlayerControll()
+    {
+        dir = 0;
+
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            dir += -1;
+        }
+        if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            dir += 1;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(dir * speed, rb.linearVelocity.y);
     }
 }
