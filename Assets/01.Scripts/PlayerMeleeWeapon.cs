47922a0f100bdbc7d512b7899ea207ffe6380c7a
RFinex <leo97kr@gmail.com>
2026-07-13 11:13:44 +0900

    feat: Add Scripts

---
+++ Assets/01.Scripts/PlayerMeleeWeapon.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerMeleeWeapon : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/01.Scripts/PlayerWeapon.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
