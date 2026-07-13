6481f97cd2b183365201634335699f16037ed57c
RFinex <leo97kr@gmail.com>
2026-07-13 17:51:42 +0900

    feat: Add Camera Move

--- Assets/01.Scripts/CameraMove.cs
+++ Assets/01.Scripts/CameraMove.cs
@@ -10,14 +10,28 @@ public class CameraMove : MonoBehaviour
     private void Awake()
     {
         camera = Camera.main;
-    }
-    void Start()
-    {
-        
+        screenHalfSizeY = camera.orthographicSize;
+        screenHalfSizeX = screenHalfSizeY * camera.aspect;
     }
 
-    void Update()
+    private void LateUpdate()
     {
-        
+        if (transform.position.x < target.position.x - screenHalfSizeX)
+        {
+            transform.position += Vector3.right * screenHalfSizeX * 2;
+        }
+        else if (transform.position.x > target.position.x + screenHalfSizeX)
+        {
+            transform.position += Vector3.left * screenHalfSizeX * 2;
+        }
+
+        if (transform.position.y < target.position.y - screenHalfSizeY)
+        {
+            transform.position += Vector3.up * screenHalfSizeY * 2;
+        }
+        else if (transform.position.y > target.position.y + screenHalfSizeY)
+        {
+            transform.position += Vector3.down * screenHalfSizeY * 2;
+        }
     }
 }
