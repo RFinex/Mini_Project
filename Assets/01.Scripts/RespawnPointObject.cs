79254fb285df37086383243cbeb42209514a64dc
RFinex <leo97kr@gmail.com>
2026-07-15 11:40:22 +0900

    feat: Add respawn point object

---
+++ Assets/01.Scripts/RespawnPointObject.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class RespawnPointObject : MonoBehaviour
+{
+    private GameObject respawnPoint;
+
+    private void Awake()
+    {
+        respawnPoint = GameObject.Find("RespawnPoint");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            respawnPoint.transform.position = transform.position;
+        }
+    }
+}
