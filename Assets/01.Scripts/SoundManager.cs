ebd835f87d7bae74f6b8eee83726203e6003aaef
RFinex <leo97kr@gmail.com>
2026-07-16 11:50:09 +0900

    feat: Add singleton

--- Assets/01.Scripts/SoundManager.cs
+++ Assets/01.Scripts/SoundManager.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
     void Start()
     {
         
