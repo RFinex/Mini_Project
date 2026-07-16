1b2fe82cad84b1c10090c6d4c9eeda228d76f8dc
RFinex <leo97kr@gmail.com>
2026-07-16 16:38:39 +0900

    feat: Add launch item dir guide

--- Assets/01.Scripts/LaunchItem.cs
+++ Assets/01.Scripts/LaunchItem.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class LaunchItem : Item
 {
+    [SerializeField] private GameObject dirGuide;
+
     private void Awake()
     {
         wait = new WaitForSeconds(delay);
         col = GetComponent<Collider2D>();
         sr = GetComponent<SpriteRenderer>();
+        dirGuide = transform.GetChild(0).gameObject;
     }
 
     protected override void OnTriggerEnter2D(Collider2D collision)
@@ -14,6 +17,15 @@ public class LaunchItem : Item
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             collision.GetComponent<PlayerController>().LaunchPlayer(transform.position);
+            dirGuide.SetActive(true);
+        }
+    }
+
+    protected void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            dirGuide.SetActive(false);
             StartCoroutine(ItemRespawn());
         }
     }
