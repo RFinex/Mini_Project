5f7742c40f59e22a97a0aa43f5da0ae7ec1c7619
RFinex <leo97kr@gmail.com>
2026-07-19 22:23:57 +0900

    feat: Add boss controller

---
+++ Assets/01.Scripts/BossController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    private int maxHp;
+    private int nowHp;
+
+    private float moveSpeed;
+    private Transform target;
+
+    private SpriteRenderer sr;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        moveSpeed = 3f;
+
+        target = GameObject.Find("Player").transform;
+    }
+
+    private void OnEnable()
+    {
+        maxHp = 100;
+        nowHp = 100;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private bool CheckFlip()
+    {
+        return transform.position.x > target.position.x ? true : false;
+    }
+}
