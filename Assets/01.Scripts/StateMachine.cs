f960eb81685748b9a8d251ee3fd740b925082964
RFinex <leo97kr@gmail.com>
2026-07-20 10:56:25 +0900

    feat: Update State Machine

--- Assets/01.Scripts/StateMachine.cs
+++ Assets/01.Scripts/StateMachine.cs
@@ -1,14 +1,31 @@
 using UnityEngine;
 
-public class StateMachine : MonoBehaviour
+public interface IState<T>
 {
-    void Start()
+    public void Enter(T obj);
+
+    public void Exit(T obj);
+
+    public void Update(T obj);
+}
+
+public class StateMachine<T>
+{
+    protected IState<T> currentState;
+
+    private T obj;
+
+    public void ChangeState(IState<T> state)
     {
-        
+        if (currentState != null)
+            currentState.Exit(obj);
+
+        currentState = state;
+        currentState.Enter(obj);
     }
 
-    void Update()
+    public void Update()
     {
-        
+        currentState.Update(obj);
     }
 }
