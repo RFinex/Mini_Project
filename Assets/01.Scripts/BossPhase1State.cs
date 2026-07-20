338f0c92af03fff4e453a428f6228d2203cf6200
RFinex <leo97kr@gmail.com>
2026-07-20 11:35:48 +0900

    chore: Update PhaseState

--- Assets/01.Scripts/BossPhase1State.cs
+++ Assets/01.Scripts/BossPhase1State.cs
@@ -4,16 +4,16 @@ public class BossPhase1State : IState<BossController>
 {
     public void Enter(BossController obj)
     {
-        throw new System.NotImplementedException();
+        
     }
 
     public void Exit(BossController obj)
     {
-        throw new System.NotImplementedException();
+        
     }
 
     public void Update(BossController obj)
     {
-        throw new System.NotImplementedException();
+        
     }
 }
