0d8a2fdc4148bd3c02cf1faeba0f314e8f50e92c
RFinex <leo97kr@gmail.com>
2026-07-21 16:59:08 +0900

    fix: Fix laser collider bug

--- Assets/01.Scripts/MiniBossHeavyAttackState.cs
+++ Assets/01.Scripts/MiniBossHeavyAttackState.cs
@@ -8,12 +8,14 @@ public class MiniBossHeavyAttackState : IState<MiniBossController>
     {
         timer = 0f;
         int nextPattern = Random.Range(0, 3);
+        obj.MBAnimator.SetBool(obj.IsHeavy, true);
         obj.HeavyAttack(nextPattern);
     }
 
     public void Exit(MiniBossController obj)
     {
         timer = 0f;
+        obj.MBAnimator.SetBool(obj.IsHeavy, false);
     }
 
     public void Update(MiniBossController obj)
