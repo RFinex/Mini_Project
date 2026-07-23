ce43883f1fa2b03d113ccd00cd6c450801e3832d
RFinex <leo97kr@gmail.com>
2026-07-23 16:23:32 +0900

    fix: Fix attack timing

--- Assets/01.Scripts/BossPhase1Pattern.cs
+++ Assets/01.Scripts/BossPhase1Pattern.cs
@@ -24,16 +24,17 @@ public class BossPhase1Pattern : BossPatternBase
         angle = 30f;
         for (int i = 0; i < 5; i++)
         {
+            yield return wait;
             for (int j = 0; j < 12; j++)
-            {
-                yield return wait;
+            {                
                 GameObject fire = ObjectPoolManager.instance.GetObject("bossBullet");
                 fire.transform.position = boss.AttackPos.position;
                 fire.GetComponent<BossBullet>().SetDirection(boss.GetAttackPosDirection());
                 fire.transform.rotation = Quaternion.Euler(0f, 0f, j * angle);
             }
+            yield return wait2;
         }
-        yield return null;
+        
         isFinish = true;
     }
 
