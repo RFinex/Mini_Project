eec39a32dc2698d0ed19ec498436d5b825c9717b
RFinex <leo97kr@gmail.com>
2026-07-27 16:09:56 +0900

    feat: Update slime

---
+++ Assets/01.Scripts/MonsterAttackState.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MonsterAttackState : IState<EnemyController>
+{
+    public void Enter(EnemyController obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Exit(EnemyController obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Update(EnemyController obj)
+    {
+        throw new System.NotImplementedException();
+    }
+}
