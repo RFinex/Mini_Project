6482fceb0c7f2ca9934f600a896ddf7575008d4a
RFinex <leo97kr@gmail.com>
2026-07-27 15:40:35 +0900

    assets: Add slime

---
+++ Assets/01.Scripts/Slime.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Slime : EnemyController
+{
+    private StateMachine<EnemyController> stateMachine;
+
+    public override Vector2 GetDirection()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ReturnPool()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void TakeDamage()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void CheckFlip()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Die()
+    {
+        throw new System.NotImplementedException();
+    }
+}
